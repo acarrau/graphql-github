89bd23ccf906aa356272a80bdf85f807dcf4b3d8
Alejandro Carrau <193284+acarrau@users.noreply.github.com>
2017-09-20 17:09:13 -0300

    Add RepoType

--- schema.js
+++ schema.js
@@ -2,8 +2,28 @@ const fetch = require('node-fetch');
 const {
     GraphQLSchema,
     GraphQLObjectType,
+    GraphQLList,
     GraphQLString
 } = require('graphql');
+
+const RepoType = new GraphQLObjectType({
+    name: 'Repo',
+    description: 'Repo',
+    fields: {
+        name: {
+            type: GraphQLString,
+            resolve: data => data.name
+        },
+        url: {
+            type: GraphQLString,
+            resolve: data => data.url
+        },
+        size: {
+            type: GraphQLString,
+            resolve: data => data.size
+        }
+    }
+})
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -20,6 +40,14 @@ const UserType = new GraphQLObjectType({
         avatar_url: {
             type: GraphQLString,
             resolve: data => data.avatar_url
+        },
+        repos: {
+            type: new GraphQLList(RepoType),
+            resolve: (root, args) => {
+                console.log("root", root, args)
+                return fetch(`https://api.github.com/users/${args.userName}/repos`)
+                    .then(response => response.json())
+            }
         }
     }
 })
